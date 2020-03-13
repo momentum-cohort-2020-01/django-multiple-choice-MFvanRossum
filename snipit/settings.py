fa06ca0af33bcce346f8780dabc1078838fd5ee8
MFvanRossum <matissev@hotmail.com>
2020-03-13 11:47:37 -0400

    Deleted sqlite from .env. Maybe this will work?

--- snipit/settings.py
+++ snipit/settings.py
@@ -169,4 +169,4 @@ INTERNAL_IPS = [
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
-del DATABASES['default']['OPTIONS']['sslmode']
+# del DATABASES['default']['OPTIONS']['sslmode']
